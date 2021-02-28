09dfef694c170821a85d838439644dda5822a85c
ComplexScripts <73372033+ComplexScripts@users.noreply.github.com>
2021-02-28 04:55:06 -0600

    Update tp_to_top.lua

--- toh/tp_to_top.lua
+++ toh/tp_to_top.lua
@@ -1,3 +1,3 @@
 local plr = game.Players.LocalPlayer
 
-plr.Character.HumanoidRootPart.CFrame = CFrame.new(97.5640411, 357.249512, -40.7397308)
+plr.Character.HumanoidRootPart.CFrame = game.workspace.tower.sections.finish.exit.carpet.CFrame * CFrame.Angles(math.rad(90), math.rad(0), math.rad(90)) + Vector3.new(0, 0.2, 0)
