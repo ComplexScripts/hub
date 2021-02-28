12d4733687394aeafe62f3913b9c8c95c1f8c55d
ComplexScripts <73372033+ComplexScripts@users.noreply.github.com>
2021-02-28 15:50:39 -0600

    Delete boost.lua

--- toh/boost.lua
+++
@@ -1,9 +0,0 @@
-local plr = game.Players.LocalPlayer
-
-_G.Speed = 100
-_G.Jump = 150
-
-if _G.Speed and _G.Jump then
-    plr.Character.Humanoid.WalkSpeed = _G.Speed
-    plr.Character.Humanoid.JumpPower = _G.Jump
-end
