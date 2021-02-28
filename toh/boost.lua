a5d9695cec76756045f223dff30e4c2d77a41ab1
ComplexScripts <73372033+ComplexScripts@users.noreply.github.com>
2021-02-28 04:37:13 -0600

    Update boost.lua

--- toh/boost.lua
+++ toh/boost.lua
@@ -4,8 +4,6 @@ _G.Speed = 100
 _G.Jump = 150
 
 if _G.Speed and _G.Jump then
-    while wait() do
-        plr.Character.Humanoid.WalkSpeed = _G.Speed
-        plr.Character.Humanoid.JumpPower = _G.Jump
-    end
+    plr.Character.Humanoid.WalkSpeed = _G.Speed
+    plr.Character.Humanoid.JumpPower = _G.Jump
 end
